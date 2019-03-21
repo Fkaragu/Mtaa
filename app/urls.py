791ba4783764950a0313f42d21d8a395694c313f
Francis Karagu <fkaragu@gmail.com>
2019-03-21 15:28:22 +0300

    Added index css

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.welcome),
@@ -9,3 +10,5 @@ urlpatterns = [
     url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
     url(r'^editprofile/',views.editprofile, name='editprofile')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
