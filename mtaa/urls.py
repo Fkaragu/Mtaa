e770c2d440a9106a1b1aeee2920f958de445dd71
Francis Karagu <fkaragu@gmail.com>
2019-03-21 12:51:36 +0300

    Modified url

--- mtaa/urls.py
+++ mtaa/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'', include('app.urls')),
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url('^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.logout, {"next_page": '/'})
 ]
