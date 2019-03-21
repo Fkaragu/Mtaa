375809a0d73298034d41b645f58a91acf90bae96
Francis Karagu <fkaragu@gmail.com>
2019-03-21 11:35:20 +0300

    Added psycopg

--- mtaa/settings.py
+++ mtaa/settings.py
@@ -58,8 +58,10 @@ WSGI_APPLICATION = 'mtaa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mtaa',
+        'USER': 'francs',
+    'PASSWORD':'master',
     }
 }
 
