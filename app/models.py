68b53f12f7a7ac0445547434d9785e881a6f3afb
Francis Karagu <fkaragu@gmail.com>
2019-03-21 16:25:15 +0300

    Added dashboard link

--- app/models.py
+++ app/models.py
@@ -14,3 +14,27 @@ class Profile(models.Model):
 
     def save_profile(self):
         self.save()
+
+class Neighbourhood(models.Model):
+    name = models.TextField(max_length=30, blank=True)
+    location = models.TextField(max_length=30, blank=True)
+    occupants = models.TextField(max_length=30, blank=True)
+    user = models.OneToOneField(User)
+
+    def __str_(self):
+        return self.name
+
+    def neighbourhood(self):
+        self.save()
+
+class Business(models.Model):
+    name = models.TextField(max_length=30, blank=True)
+    user = models.ForeignKey(User)
+    neighbourhood = models.ForeignKey(Neighbourhood)
+    email = models.EmailField(max_length=70,blank=True)
+
+    def __str_(self):
+        return self.name
+
+    def save_business(self):
+        self.save()
