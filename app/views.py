71464601bac65485967be98d8fa93b40810710d9
Francis Karagu <fkaragu@gmail.com>
2019-03-21 13:03:02 +0300

    Added register function

--- app/views.py
+++ app/views.py
@@ -4,3 +4,18 @@ from .models import *
 
 def welcome(request):
     return render(request, 'master/index.html')
+
+def register(request):
+    if request.user.is_authenticated():
+        return render(request,'master/index.html')
+    else:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = True
+                user.save()
+                return redirect(request, 'Login/success.html')
+        else:
+            form = SignupForm()
+            return render(request, 'Login/signup.html',{'form':form})
